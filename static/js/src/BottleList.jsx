6080e610be976233a3a45ea379ddb97b05801972
Atle Frenvik Sveen <atle@frenviksveen.net>
2015-01-26 23:34:26 +0100

    format dates

--- static/js/src/BottleList.jsx
+++ static/js/src/BottleList.jsx
@@ -1,19 +1,42 @@
 var Cellar = this.Cellar || {};
 (function (ns) {
     'use strict';
+
+    function formatDate(date) {
+        if (date) {
+            return moment(date).format('DD.MM.YYYY');    
+        }
+        return '';        
+    }
     
     var Bottle = React.createClass({
         render: function () {
             return (
                 <tr>
-                    <td className="td-20">{this.props.bottle.breweryName}</td>
-                    <td className="td-20">{this.props.bottle.beerName}</td>
-                    <td className="td-10">{this.props.bottle.batchNo}</td>
-                    <td className="td-10">{this.props.bottle.brewDate}</td>
-                    <td className="td-10">{this.props.bottle.bbfDate}</td>
-                    <td className="td-5">{this.props.bottle.size}</td>
-                    <td className="td-5">{this.props.bottle.amount}</td>
-                    <td className="td-20">{this.props.bottle.comment}</td>
+                    <td className="td-20">
+                        {this.props.bottle.breweryName}
+                    </td>
+                    <td className="td-20">
+                        {this.props.bottle.beerName}
+                    </td>
+                    <td className="td-10">
+                        {this.props.bottle.batchNo}
+                    </td>
+                    <td className="td-10">
+                        {formatDate(this.props.bottle.brewDate)}
+                    </td>
+                    <td className="td-10">
+                        {formatDate(this.props.bottle.bbfDate)}
+                    </td>
+                    <td className="td-5">
+                        {this.props.bottle.size}
+                    </td>
+                    <td className="td-5">
+                        {this.props.bottle.amount}
+                    </td>
+                    <td className="td-20">
+                        {this.props.bottle.comment}
+                    </td>
                 </tr>
             );
         }
--- static/js/src/gen/BottleList.js
+++ static/js/src/gen/BottleList.js
@@ -1,19 +1,42 @@
 var Cellar = this.Cellar || {};
 (function (ns) {
     'use strict';
+
+    function formatDate(date) {
+        if (date) {
+            return moment(date).format('DD.MM.YYYY');    
+        }
+        return '';        
+    }
     
     var Bottle = React.createClass({displayName: "Bottle",
         render: function () {
             return (
                 React.createElement("tr", null, 
-                    React.createElement("td", {className: "td-20"}, this.props.bottle.breweryName), 
-                    React.createElement("td", {className: "td-20"}, this.props.bottle.beerName), 
-                    React.createElement("td", {className: "td-10"}, this.props.bottle.batchNo), 
-                    React.createElement("td", {className: "td-10"}, this.props.bottle.brewDate), 
-                    React.createElement("td", {className: "td-10"}, this.props.bottle.bbfDate), 
-                    React.createElement("td", {className: "td-5"}, this.props.bottle.size), 
-                    React.createElement("td", {className: "td-5"}, this.props.bottle.amount), 
-                    React.createElement("td", {className: "td-20"}, this.props.bottle.comment)
+                    React.createElement("td", {className: "td-20"}, 
+                        this.props.bottle.breweryName
+                    ), 
+                    React.createElement("td", {className: "td-20"}, 
+                        this.props.bottle.beerName
+                    ), 
+                    React.createElement("td", {className: "td-10"}, 
+                        this.props.bottle.batchNo
+                    ), 
+                    React.createElement("td", {className: "td-10"}, 
+                        formatDate(this.props.bottle.brewDate)
+                    ), 
+                    React.createElement("td", {className: "td-10"}, 
+                        formatDate(this.props.bottle.bbfDate)
+                    ), 
+                    React.createElement("td", {className: "td-5"}, 
+                        this.props.bottle.size
+                    ), 
+                    React.createElement("td", {className: "td-5"}, 
+                        this.props.bottle.amount
+                    ), 
+                    React.createElement("td", {className: "td-20"}, 
+                        this.props.bottle.comment
+                    )
                 )
             );
         }
