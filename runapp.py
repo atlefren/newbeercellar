5edff7a57dc006dbd0aea4cf536575afd033a51b
Atle Frenvik Sveen <atle@frenviksveen.net>
2015-02-08 13:06:54 +0100

    another try 4

--- runapp.py
+++ runapp.py
@@ -1,4 +1,3 @@
-'''
 import os
 from newbeercellar import app
 
@@ -6,16 +5,3 @@ from newbeercellar import app
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
-'''
-
-from flask import Flask
-app = Flask(__name__)
-
-
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-
-if __name__ == "__main__":
-    app.run()
